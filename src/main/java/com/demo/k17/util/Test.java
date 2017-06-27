b9cf04103b89c7cc1eb7ead244909abd355c43f0
Liko <13764384658@163.com>
2017-06-27 18:21:37 +0800

    初步抓取（存储结构不明）

--- src/main/java/com/demo/k17/util/Test.java
+++ src/main/java/com/demo/k17/util/Test.java
@@ -12,9 +12,27 @@ import java.io.IOException;
  */
 public class Test {
     public static void main(String[] args) throws IOException {
-        Connection con = Jsoup.connect("http://www.17k.com/chapter/2401966/28020958.html");
+        bookMenu();
+         Connection con = Jsoup.connect("http://www.17k.com/chapter/2477255/28942975.html");
         Document doc = con.get();
-        Elements content = doc.select("#chapterContentWapper");
-        System.out.println(content);
+        Elements content = doc.select("div#chapterContentWapper");
+        String text = content.text();
+        String [] s=text.split("\\s+?");
+      for (String ss:s) {
+          System.out.println(ss);
+        }
+
+    }
+    //Main List
+
+    static void bookMenu() throws IOException {
+        Connection con = Jsoup.connect("http://www.17k.com/list/2477255.html");
+        Document doc = con.get();
+        Elements content = doc.select("div.Main.List dl.Volume dd");
+        String text = content.text();
+        String [] s=text.split("\\s+");
+        for (String ss:s) {
+            System.out.println(ss);
+        }
     }
 }
