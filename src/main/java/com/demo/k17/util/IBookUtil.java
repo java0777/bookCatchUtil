e0e26b0355226122fab9c69d8fb8b7a1161a2a3b
王工头 <591674141@qq.com>
2017-06-30 10:39:21 +0800

    定义抓取组件实现接口

---
+++ src/main/java/com/demo/k17/util/IBookUtil.java
@@ -0,0 +1,58 @@
+package com.demo.k17.util;
+
+import org.jsoup.nodes.Document;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 17k网站 书籍抓取工具类
+ */
+public interface IBookUtil {
+
+    /**
+     * get方式抓取txt内容网址
+     *
+     * @param url    网址
+     * @param params 参数
+     * @return doc对象
+     */
+    Document getTextDoc(String url, Map<String, String> params);
+
+    /**
+     * post方式抓取txt内容网址
+     *
+     * @param url    网址
+     * @param params 参数
+     * @return doc对象
+     */
+    Document getPostDoc(String url, Map<String, String> params);
+
+    /**
+     * 根据章节集合内容网址 获取整张页面内容
+     * @return 抓取获得的doc对象
+     */
+    Document getTitlesDoc();
+
+    /**
+     * 根据抓取到的doc对象，解析出章节相关内容
+     *
+     * @param doc doc对象
+     * @return key为中文章节名，value为对应的url
+     */
+    List<Map<String, String>> getAllTitles(Document doc);
+
+    /**
+     * 根据章节文字内容网址 获取整张页面内容
+     * @return 抓取获得的doc对象
+     */
+    Document getContentDoc();
+
+    /**
+     * 根据章节文字内容doc对象 获取有效数据
+     * @param titleContentDoc
+     * @return  文字信息的缓存对象
+     */
+    StringBuilder  getTitleContent(Document titleContentDoc);
+
+}
