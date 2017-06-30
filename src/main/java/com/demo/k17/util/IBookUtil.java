3d69522e6712b130d0c14e3457188068295c19a5
王工头 <591674141@qq.com>
2017-06-30 15:29:19 +0800

    最难抓取业务部分

--- src/main/java/com/demo/k17/util/IBookUtil.java
+++ src/main/java/com/demo/k17/util/IBookUtil.java
@@ -57,4 +57,15 @@ public interface IBookUtil {
      */
     StringBuilder  getTitleContent(Document titleContentDoc);
 
+    /**
+     * 根据作者，书籍名，调用起点或17k查询接口，得到解析后的章节内容
+     * 1.通过接口判断是否有该本书
+     * 没有 返回null
+     * 有 一步步嵌套解析得到所得章节内容
+     * Map  key 章节名  value 解析后的章节内容
+     * @param author 作者
+     * @param bookName   书籍名
+     * @return  null没有此书
+     */
+    Map<String,StringBuilder> getAllTitles(String author,String bookName);
 }
