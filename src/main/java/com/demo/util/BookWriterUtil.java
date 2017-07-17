485b78d58702acd60f5401c823ad83ab13f76988
王工头 <591674141@qq.com>
2017-07-17 11:54:03 +0800

    修复存储文件字符编码错误

--- src/main/java/com/demo/util/BookWriterUtil.java
+++ src/main/java/com/demo/util/BookWriterUtil.java
@@ -1,9 +1,6 @@
 package com.demo.util;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.util.List;
 import java.util.Map;
 import java.util.regex.Matcher;
@@ -20,30 +17,30 @@ public class BookWriterUtil {
      * @param workResult   章节内容对象
      * @param titleRegex   章节调整正则
      */
-    public  static void writerAllBook(String  allBookTxtPath,String bookFileName, List<Map<String, Object>> workResult, String titleRegex) {
+    public static void writerAllBook(String allBookTxtPath, String bookFileName, List<Map<String, Object>> workResult, String titleRegex) {
         try {
             File path = new File(allBookTxtPath);
-            if (!path.exists()){
+            if (!path.exists()) {
                 path.mkdirs();
             }
-            String bookFile=allBookTxtPath + bookFileName + ".txt";
+            String bookFile = allBookTxtPath + bookFileName + ".txt";
             File file = new File(bookFile);
-            if (file.exists()){
+            if (file.exists()) {
                 file.delete();
             }
-            FileOutputStream out = new FileOutputStream(bookFile, true);
+            BufferedWriter writer = getWriter(bookFile);
             for (Map item : workResult) {
                 String titleName = item.get("name").toString().trim();
                 titleName = getTitle(titleName, titleRegex);
-                out.write(titleName.getBytes());
+                writer.write(titleName);
                 String content = item.get("content").toString().trim();
                 String[] lines = content.split(" ");
                 for (String line : lines) {
-                    line += System.getProperty("line.separator");
-                    out.write(line.getBytes());
+                    writer.newLine();
+                    writer.write(line);
                 }
             }
-            out.close();
+            writer.close();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
@@ -51,11 +48,32 @@ public class BookWriterUtil {
         }
     }
 
-    public  static void fileWriter(String file,String content) throws IOException {
-        FileOutputStream out = new FileOutputStream(file);
-        out.write(content.getBytes());
-        out.close();
+    /**
+     * 根据完整文件名，创建字符编码为UTF-8的字符输出流
+     *
+     * @param file 完整文件名，包含路径
+     * @return
+     * @throws IOException
+     */
+    public static BufferedWriter getWriter(String file) throws IOException {
+        FileOutputStream out = new FileOutputStream(file, true);
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(out, "UTF-8"));
+        return writer;
     }
+
+    /**
+     * 根据完整文件名，创建字符编码为UTF-8的字符输入流
+     *
+     * @param file 完整文件名，包含路径
+     * @return
+     * @throws IOException
+     */
+    public static BufferedReader getReader(String file) throws IOException {
+        FileInputStream input = new FileInputStream(file);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(input, "UTF-8"));
+        return reader;
+    }
+
     /**
      * 获得调整后的章节名
      *
@@ -68,7 +86,7 @@ public class BookWriterUtil {
     public static String getTitle(String titleName, String titleRegex) {
         Pattern regex = null;
         if (titleRegex != null) {
-            regex=Pattern.compile(titleRegex);
+            regex = Pattern.compile(titleRegex);
         }
         if (regex == null) {
             titleName += System.getProperty("line.separator");
